643970b0612817d510af0792a815fb97cc1e0569
Aleister Coinley <coinyon@uberhax0r.de>
2020-10-31 16:34:04 +0100

    Add staking test for mainnet fork by buying HEX first

--- tests/test_liquidstake_live.py
+++ tests/test_liquidstake_live.py
@@ -4,15 +4,20 @@ import brownie
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def hex_contract(Contract):
     # deploy the contract with the initial value as a constructor argument
     yield Contract.from_explorer("0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39")
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
+def uniswap_v1_hex(Contract):
+    yield Contract.from_explorer("0x05cde89ccfa0ada8c88d5a23caaa79ef129e7883")
+
+
+@pytest.fixture(scope="session")
 def liquidstake_contract(liquidstake, hex_contract, accounts):
-    # deploy the contract with the initial value as a constructor argument
+    # deploy the contract with the original hex contract as a constructor argument
     yield liquidstake.deploy(hex_contract, {'from': accounts[0]})
 
 
@@ -21,6 +26,24 @@ def test_balance(hex_contract, accounts):
 
 
 def test_stake_without_any_hex(hex_contract, liquidstake_contract, accounts):
+    assert hex_contract.balanceOf(accounts[0]) == 0
     hex_contract.approve(liquidstake_contract, 100 * 1e8, {'from': accounts[0]})
     with brownie.reverts():
         liquidstake_contract.stake(100 * 1e8, 365, {'from': accounts[0]})
+
+
+def test_buy_and_stake_hex(hex_contract, uniswap_v1_hex, liquidstake_contract, accounts):
+    # Buy at least 100 HEX (in the future we might need to use uniswap V2 here)
+    uniswap_v1_hex.ethToTokenSwapInput(
+        100 * 1e8,  # minimum amount of tokens to purchase
+        9999999999,  # timestamp
+        {
+            "from": accounts[0],
+            "value": "1 ether"
+        }
+    )
+
+    # Stake 100 HEX
+    assert hex_contract.balanceOf(accounts[0]) >= 100 * 1e8
+    hex_contract.approve(liquidstake_contract, 100 * 1e8, {'from': accounts[0]})
+    liquidstake_contract.stake(100 * 1e8, 365, {'from': accounts[0]})
